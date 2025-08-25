bf46602947b2bde058903611310350f45b6ba844
Mena-Okinedo <menaokinedo@gmail.com>
2025-08-25 19:43:03 +0100

    Update TemperatureConverter.js

---
+++ TemperatureConverter.js
@@ -0,0 +1,35 @@
+// function to convert Celsius to fahrenheit
+
+function CelsiusToFahrenheit (Celsius) {
+    return(Celsius * 9/5) + 32;}
+
+// Convert Celsius to Kelvin
+
+function celsiusToKelvin(celsius) {
+    return celsius + 273.15;
+}
+
+// Convert Fahrenheit to Celsius
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+// Convert Fahrenheit to Kelvin
+
+function fahrenheitToKelvin(fahrenheit) {
+    return (fahrenheit - 32) * 5/9 + 273.15;
+}
+
+// Convert Kelvin to Celsius
+
+function kelvinToCelsius(kelvin) {
+    return kelvin - 273.15;
+}
+
+// Convert Kelvin to Fahrenheit
+
+function kelvinToFahrenheit(kelvin) {
+    return (kelvin - 273.15) * 9/5 + 32;
+}
+
